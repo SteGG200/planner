a36a785c4027a6c4efed00c8c64922317f7de617
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-01-04 23:37:06 +0700

    Add logging

--- server/utils/create_body.go
+++ server/utils/create_body.go
@@ -1,11 +1,15 @@
 package utils
 
-func CreateBody(data string) map[string]interface{} {
+import "log"
+
+func CreateBody(message string) map[string]interface{} {
+	log.Printf("creating body with message %s", message)
+
 	return map[string]interface{}{
 		"model": "gpt-3.5-turbo",
 		"messages": []interface{}{map[string]interface{}{
 			"role":    "system",
-			"content": data,
+			"content": message,
 		}},
 	}
 }

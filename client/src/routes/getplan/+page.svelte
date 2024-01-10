83e5d75b631c7ad418e2f75e75d46acacedb929b
Boi-from-nowhere <nguyendangdung1122007@gmail.com>
2024-01-10 20:38:27 +0700

    getplan

--- client/src/routes/getplan/+page.svelte
+++ client/src/routes/getplan/+page.svelte
@@ -1,48 +1,34 @@
-<div>
-	<div class="text-center font-sans text-[32px]">
-		<h1 class="mb-0">4 Crucial Steps of</h1>
-	</div>
-	
-	<div class="text-center font-serif text-[22px]">
-	  <p class="mt-0">Achieving 7.0 ielts score</p>
-	</div>
-	<div class="plan">
-	  <div class="flex flex-wrap font-mono text-[20px]">
-		  <div class="m-auto p-[20px] text-[30px] w-[500px]">
-			<p><b>1. Understanding the Test: </b></p>
-			<ul class="font-mono text-[14px]">
-			  <li>Familiarize Yourself: Understand the structure of the IELTS test, including Listening, Reading, Writing, and Speaking sections..</li>
-			  <li>Know the Format: Get acquainted with the types of questions in each section.</li>
-			</ul>
-		  </div>
-		  
-		  
-		  <div class="m-auto p-[20px] text-[30px] w-[500px]">
-			<p><b>2. Effective Preparation:</b></p>
-			<ul class="font-mono text-[14px]">
-			  <li>Regular Practice: Utilize official IELTS practice materials regularly to get accustomed to the test format.</li>
-			  <li>Improve Skills: Enhance overall English proficiency, focusing on grammar, vocabulary, pronunciation, and fluency.</li>
-			</ul>
-		  </div>
-		  
-		  <div class="m-auto p-[20px] text-[30px] w-[500px]">
-			<p><b>3. Targeted Sectional Practice:</b></p>
-			<ul class="font-mono text-[14px]">
-			  <li>Listening and Reading: Practice note-taking and develop strategies to understand main ideas and details.</li>
-			  <li>Writing: Practice timed essay and report writing, emphasizing organization, grammar, and vocabulary.</li>
-			  <li>Speaking: Engage in regular spoken English practice, record yourself, and work on clear expression.</li>
-			</ul>
-		  </div>
-		  
-		  <div class="m-auto p-[20px] text-[30px] w-[500px]">
-			<p><b>4. Strategic Test-Taking:</b></p>
-			<ul class="font-mono text-[14px]">
-			  <li>Time Management: Practice time management during each section to ensure completion within the allotted time.</li>
-			  <li>Feedback and Correction: Seek feedback on practice tests and essays to identify and correct mistakes.</li>
-			  <li>Stay Calm: Develop relaxation techniques to manage test anxiety and familiarize yourself with the test center procedures.</li>
-			</ul>
-		  </div>
+<script>
+	import { onMount } from 'svelte';
 
-		</div>
+	let Data : any[];
+
+	onMount (async () =>{
+		const resp = await fetch ('http://localhost:8000/blog');
+		Data = await resp.json();
+		console.log(Data.length);
+	});
+</script>
+
+
+<div>
+	<div class="pt-[50px] text-center font-sans text-[32px]">
+		<h1 class="mb-0">{Data.length} Crucial Steps of</h1>
 	</div>
+	<div class="pb-[30px] text-center font-serif text-[22px]">
+		<p class="mt-0">Achieving gold medal IOI</p>
+		<!-- <p class="mt-0">{usergoal}</p> -->
+	</div>
+
+	
+	<div class="plan">
+		<div class="flex flex-wrap font-mono text-[20px]">
+			{#each Data as {time, plan}}
+				<div class="m-auto p-[20px] text-[30px] w-[450px]">
+					<p><b>{time}</b></p>
+					<p class="font-mono text-[14px]"> {plan} </p>
+				</div>
+			{/each}
+			</div>
+		</div>
 </div>

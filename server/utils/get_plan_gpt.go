5a97ef040225b372a5345010a47afd7486ae4cb8
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-01-13 22:45:42 +0700

    Alter get plan command

--- server/utils/get_plan_gpt.go
+++ server/utils/get_plan_gpt.go
@@ -17,8 +17,8 @@ func GetPlanGpt(usergoal string, time string, information []Query) (string, erro
 	information_json, _ := json.Marshal(information)
 
 	body_req := CreateBody(
-		"You are a planner. User has a goal is: \""+Escape(usergoal)+"\" and you have just got information about user's goal with the conversation in json format: "+string(information_json)+".Divide "+time+" into maximum 10 interval time and tell user what they should do in each time to achieve their goal (just send a json format like this example [{\"time\": \"Year a, Month b, Week c,...\", \"plan\": \"plan in that interval time...\"}...]",
-		0.5,
+		"You are a planner assistant. User has a goal is: \""+Escape(usergoal)+"\". They have exact "+time+" to achieve the goal no more no less and there are some extra information about user is provided in this json format: "+string(information_json)+".Divide "+time+" into maximum 10 periods. All the period do not need to have the same time interval. Tell user what they should do in each period and list the books and websites they could use for achieve the goal in each period (if it's a link, please write it with http or https. For example: https://facebook.com). (Just send a json format the exact same as this example [{\"time\": \"Year a, Month b,... (if time is year you should divide like year a, month b; else if time is months you should divide like month a, week b; else if time is week you should divide like week a, day b. Remember a year has 12 months, a months has 4 weeks, a week has 7 days. Please don't across these limits)\", \"plan\": \"plan in that period...\", \"resourses\":\" 1) ...  2) ...  \"},...] and between 2 elements of the array mustn't have new line character).",
+		0.2,
 	)
 	buffer := new(bytes.Buffer)
 	json.NewEncoder(buffer).Encode(body_req)

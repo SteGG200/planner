ad6332c65dd9c5addaa62f77f9b515080140f423
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-01-14 14:11:30 +0700

    Change the logging

--- server/api/getplan.go
+++ server/api/getplan.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"planner/utils"
 )
@@ -14,14 +15,16 @@ func GetPlanHandler() http.Handler {
 			bodyBytes, _ := io.ReadAll(r.Body)
 			var body utils.UserInfo
 			err := json.Unmarshal(bodyBytes, &body)
+			log.Println("Received user's information successfully")
 			if err != nil {
 				fmt.Fprintf(w, "Error: %s", err.Error())
 			}
-			questions, err := utils.GetPlanGpt(body.Usergoal, body.Time, body.Queries)
+			plan, err := utils.GetPlanGpt(body.Usergoal, body.Time, body.Queries)
 			if err != nil {
 				fmt.Fprintf(w, "Error: %s", err.Error())
 			}
-			resp, _ := json.Marshal(questions)
+			log.Println("Generate the plan successfully!")
+			resp, _ := json.Marshal(plan)
 			w.Write(resp)
 		}
 	})
